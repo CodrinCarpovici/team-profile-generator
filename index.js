440c11c805664676781649f29cc9bbfb5ef9c7a5
CodrinCarpovici <gabriel_9729@yahoo.co.uk>
2024-01-23 18:11:30 +0000

    removed async block and replaced with then and catch block for clarity, created new function for menu prompt

--- index.js
+++ index.js
@@ -10,34 +10,48 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 const team = []; // team members array
 
-const gatherTeamInfo = async () => {
-    const managerInfo = await inquirer.prompt([
-        {
-            type:"input",
-            name:"name",
-            message: "Team Manager's Name:",
-        },
-        {
-            type:"input",
-            name:"id",
-            message: "Team Manager's Employee Id:",
-        },
-        {
-            type:"input",
-            name:"email",
-            message: "Team Manager's work Email address:",
-        },
-        {
-            type:"input",
-            name:"officeNumber",
-            message: "Team Manager's Office Number:",
-        },
-    ]);
+const gatherTeamInfo = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Team Manager's Name:",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Team Manager's Employee Id:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Team Manager's work Email address:",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Team Manager's Office Number:",
+      },
+    ])
+    .then((managerInfo) => {
+      const manager = new Manager(
+        managerInfo.name,
+        managerInfo.id,
+        managerInfo.email,
+        managerInfo.officeNumber
+      );
+      team.push(manager);
 
-    const manager = new Manager(managerInfo.name, managerInfo.id, managerInfo.email, managerInfo.officeNumber);
-    team.push(manager);
+      // next prompt for the rest of the team
+      menuForTeamMembers();
+    })
+    .catch((err) => console.error(err));
+};
+
+const menuForTeamMembers = () => {
+    
 }
